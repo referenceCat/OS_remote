ec750fb46a0a707f563008ca6fab3d9e16473b75
referenceCat <mi.buyanov@yandex.ru>
2024-03-04 14:17:54 +0300

    lab1 init

---
+++ lab1/task1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by referencecat on 04.03.24.
+//
+
+#include "task1.h"
+#include "iostream"
+
+int main() {
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
---
+++ lab1/task1.h
@@ -0,0 +1,8 @@
+//
+// Created by referencecat on 04.03.24.
+//
+
+#ifndef OS_TASK1_H
+#define OS_TASK1_H
+
+#endif //OS_TASK1_H
